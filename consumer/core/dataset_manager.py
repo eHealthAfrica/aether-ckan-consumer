b70e8fd8a5a1822ba5b7192de8514128bcc52305
Aleksandar Jovanov <aleksandar.jovanov@keitaro.com>
2018-04-26 08:42:08 +0200

    Refactor code

--- consumer/core/dataset_manager.py
+++ consumer/core/dataset_manager.py
@@ -2,12 +2,13 @@ import logging
 from threading import Thread
 import json
 import sys
+import os
 
 from ckanapi import RemoteCKAN
 from ckanapi import errors as ckanapi_errors
 
-from consumer.core.topic_manager import TopicManager
-from consumer.db import Resource, CkanServer
+from consumer.core.resource_manager import ResourceManager
+from consumer.config import parse_json_from_file
 
 
 class DatasetManager(Thread):
@@ -19,132 +20,89 @@ class DatasetManager(Thread):
         self.config = config
 
     def run(self):
-        self.spawn_topic_managers()
+        self.create_dataset_in_ckan()
+        self.spawn_resource_managers()
 
-    def spawn_topic_managers(self):
+    def spawn_resource_managers(self):
         dataset = self.config.get('dataset')
         dataset_name = dataset.get('metadata').get('name')
         resources = dataset.get('resources')
-        self.topic_managers = []
+        self.resource_managers = []
 
         for resource in resources:
-            self.create_resource_in_ckan(resource, dataset_name)
-            self.create_resource_in_db(resource, dataset_name)
-
-            topics = resource.get('topics')
-
-            for topic_config in topics:
-                number_of_consumers = topic_config.get('number_of_consumers')
-
-                for i in range(number_of_consumers):
-                    config = {
-                        'server_name': self.config.get('server_name'),
-                        'dataset_name': dataset_name,
-                        'topic': topic_config,
-                    }
-                    topic_manager = TopicManager(config)
-                    self.topic_managers.append(topic_manager)
-
-            if len(self.topic_managers) == 0:
-                self.logger.info('No Topic Managers spawned.')
-            else:
-                self.logger.info(
-                    'Spawned {0} Topic manager(s) for dataset {1}.'
-                    .format(len(self.topic_managers), dataset_name)
-                )
-
-        for topic_manager in self.topic_managers:
-            topic_manager.start()
-
-    def create_resource_in_db(self, resource, dataset_name):
-        metadata = resource.get('metadata')
-        resource_name = metadata.get('name')
-        ckan_server_url = self.config.get('ckan_url')
-        ckan_server = CkanServer.get_by_url(
-            ckan_server_url=ckan_server_url
-        )
-        resource = Resource.get(
-            resource_name=resource_name,
-            ckan_server_id=ckan_server.ckan_server_id,
-            dataset_name=dataset_name
-        )
-
-        if not resource:
-            data = {
-                'resource_name': resource_name,
-                'dataset_name': dataset_name,
-                'ckan_server_id': ckan_server.ckan_server_id
+            config = {
+                'dataset': dataset,
+                'resource': resource,
+                'server_name': self.config.get('server_name'),
+                'ckan_url': self.config.get('ckan_url'),
+                'api_key': self.config.get('api_key'),
             }
+            resource_manager = ResourceManager(config)
+            self.resource_managers.append(resource_manager)
 
-            Resource.create(**data)
+        if len(self.resource_managers) == 0:
+            self.logger.info('No Resource Managers spawned.')
+        else:
+            self.logger.info(
+                'Spawned {0} Resource manager(s) for dataset {1}.'
+                .format(len(self.resource_managers), dataset_name)
+            )
 
-    def create_resource_in_ckan(self, resource, dataset_name):
+        for resource_manager in self.resource_managers:
+            resource_manager.start()
+
+    def create_dataset_in_ckan(self):
         server_url = self.config.get('ckan_url')
         server_title = self.config.get('server_name')
         api_key = self.config.get('api_key')
-        title = resource.get('metadata').get('title')
-        resource_name = resource.get('metadata').get('name')
-        resource_description = resource.get('metadata').get('description')
-
-        payload = {
-            'package_id': dataset_name,
-            'name': title,
-            'description': resource_description,
-            'url_type': 'datastore'
-        }
+        metadata = self.config.get('dataset').get('metadata')
+        dataset_name = metadata.get('name')
 
         ckan = RemoteCKAN(server_url, apikey=api_key)
 
         try:
-            ckan_server = CkanServer.get_by_url(
-                ckan_server_url=server_url
-            )
-            db_resource = Resource.get(
-                resource_name=resource_name,
-                dataset_name=dataset_name,
-                ckan_server_id=ckan_server.ckan_server_id
-            )
-
-            if db_resource:
-                return
+            ckan.action.package_show(id=dataset_name)
+            return
         except ckanapi_errors.NotFound:
-            # Resource does not exist, so continue with execution to create it.
+            # Dataset does not exist, so continue with execution to create it.
             pass
 
         try:
-            response = ckan.action.resource_create(**payload)
-            resource_url = '{0}/dataset/{1}/resource/{2}'.format(
-                server_url, dataset_name, response.get('id')
-            )
-            self.logger.info('Resource {0} created in CKAN portal {1}: {2}.'
+            data = self.update_metadata_from_defaults(metadata)
+            dataset = ckan.action.package_create(**data)
+            dataset_url = '{0}/dataset/{1}'.format(server_url, dataset_name)
+            self.logger.info('Dataset {0} created in CKAN portal {1}: {2}.'
                              .format(
-                                resource_name,
+                                dataset_name,
                                 server_title,
-                                resource_url
+                                dataset_url
                              ))
         except ckanapi_errors.NotAuthorized as e:
             self.logger.error(
-                'Cannot create resource {0}. {1}'.format(
-                    resource_name,
+                'Cannot create dataset {0}. {1}'.format(
+                    dataset_name,
                     str(e),
                 )
             )
             sys.exit(1)
         except ckanapi_errors.ValidationError as e:
             self.logger.error(
-                'Cannot create resource {0}. Payload is not valid. Check the '
+                'Cannot create dataset {0}. Payload is not valid. Check the '
                 'following errors: {1}'.format(
-                    resource,
+                    dataset_name,
                     json.dumps(e.error_dict),
                 )
             )
             sys.exit(1)
 
-    def new_message_from_topic_manager(self):
-        pass
+    def update_metadata_from_defaults(self, overwritten_data):
+        dir_path = os.getcwd()
+        file_name = os.path.join('config', 'dataset_metadata.json')
 
-    def send_data_to_dataset(self):
-        pass
+        default_metadata = parse_json_from_file(dir_path, file_name)
 
-    def introspect_schema_from_message(self):
-        pass
+        updated_metadata = {}
+        updated_metadata.update(default_metadata)
+        updated_metadata.update(overwritten_data)
+
+        return updated_metadata
--- consumer/core/server_manager.py
+++ consumer/core/server_manager.py
@@ -1,16 +1,10 @@
 from time import sleep
 import logging
-import sys
-import os
 
 import requests
 from requests.exceptions import ConnectionError
-from ckanapi import RemoteCKAN
-from ckanapi import errors as ckanapi_errors
 
 from consumer.core.dataset_manager import DatasetManager
-from consumer.config import parse_json_from_file
-
 
 CONN_RETRY = 3
 CONN_RETRY_WAIT_TIME = 2
@@ -86,7 +80,6 @@ class ServerManager(object):
                 'api_key': server_config.get('api_key'),
                 'dataset': dataset,
             }
-            self.create_dataset_in_ckan(server_config, dataset)
             dataset_manager = DatasetManager(config)
             self.dataset_managers.append(dataset_manager)
 
@@ -100,59 +93,3 @@ class ServerManager(object):
 
         for dataset_manager in self.dataset_managers:
             dataset_manager.start()
-
-    def create_dataset_in_ckan(self, server_config, dataset_config):
-        server_url = server_config.get('url')
-        server_title = server_config.get('title')
-        api_key = server_config.get('api_key')
-        metadata = dataset_config.get('metadata')
-        dataset_name = metadata.get('name')
-
-        ckan = RemoteCKAN(server_url, apikey=api_key)
-
-        try:
-            ckan.action.package_show(id=dataset_name)
-            return
-        except ckanapi_errors.NotFound:
-            # Dataset does not exist, so continue with execution to create it.
-            pass
-
-        try:
-            data = self.update_metadata_from_defaults(metadata)
-            dataset = ckan.action.package_create(**data)
-            dataset_url = '{0}/dataset/{1}'.format(server_url, dataset_name)
-            self.logger.info('Dataset {0} created in CKAN portal {1}: {2}.'
-                             .format(
-                                dataset_name,
-                                server_title,
-                                dataset_url
-                             ))
-        except ckanapi_errors.NotAuthorized as e:
-            self.logger.error(
-                'Cannot create dataset {0}. {1}'.format(
-                    dataset_name,
-                    str(e),
-                )
-            )
-            sys.exit(1)
-        except ckanapi_errors.ValidationError as e:
-            self.logger.error(
-                'Cannot create dataset {0}. Payload is not valid. Check the '
-                'following errors: {1}'.format(
-                    dataset_name,
-                    json.dumps(e.error_dict),
-                )
-            )
-            sys.exit(1)
-
-    def update_metadata_from_defaults(self, overwritten_data):
-        dir_path = os.getcwd()
-        file_name = os.path.join('config', 'dataset_metadata.json')
-
-        default_metadata = parse_json_from_file(dir_path, file_name)
-
-        updated_metadata = {}
-        updated_metadata.update(default_metadata)
-        updated_metadata.update(overwritten_data)
-
-        return updated_metadata
